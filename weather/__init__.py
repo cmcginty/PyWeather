8e772ea1c88a343f1ef72a6ff8c21db422c133b7
Patrick C. McGinty <casey.mcginty@gmail.com>
2010-06-24 19:29:10 -0400

    Setup default logger to NULL output

--- weather/__init__.py
+++ weather/__init__.py
@@ -3,3 +3,12 @@ __version__ = '0.7.1'
 __doc__ = '''PyWeather branch; bindings for Davis Vantage Pro I/II weather
 stations, upload of weather data (e.g. wunderground.com), and meteorological
 calculation/conversion functions.'''
+
+import logging
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+# init a null handler, to prevent warnings
+logging.getLogger(__name__).addHandler( NullHandler() )
