66753dfa96488594431a2ae5d83c07dc414bdd25
Patrick C. McGinty <casey.mcginty@gmail.com>
2010-06-25 17:11:45 -0400

    Release v0.8

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 
 
 NAME=weather
-VER=0.7.1
+VER=0.8
 DIST_DIR=dist
 TAR=${DIST_DIR}/${NAME}-${VER}.tar.gz
 SRC_DIR=${DIST_DIR}/${NAME}-${VER}
--- weather/__init__.py
+++ weather/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.7.1'
+__version__ = '0.8'
 
 __doc__ = '''PyWeather branch; bindings for Davis Vantage Pro I/II weather
 stations, upload of weather data (e.g. wunderground.com), and meteorological
